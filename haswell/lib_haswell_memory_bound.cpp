eab0d25ba04449ec0218a9f6d0075b5daa4fad7c
John Wohlbier <john.wohlbier@engility.com>
2018-08-28 14:50:19 -0400

    - Replace tabs with spaces. - Remove unused metrics. - Do not initialize out_value to 0(.0)

--- haswell/lib_haswell_memory_bound.cpp
+++ haswell/lib_haswell_memory_bound.cpp
@@ -120,7 +120,6 @@ int haswell_membound_active_cycles(metric_id_t metric_id,
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
 
-    *out_value= 0;
     using namespace MB;
     if (ARM_MAP_MEMORY_BOUND) {
       *out_value= gEventValues.at(EventInds::CLK_UNHALTED_IND);
@@ -135,13 +134,12 @@ int haswell_membound_productive_cycles(metric_id_t metric_id,
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
 
-    *out_value= 0.0;
     using namespace MB;
 
     if (ARM_MAP_MEMORY_BOUND) {
       // The value out here is given as a fraction of active cycles
       *out_value=
-	static_cast<double>(gEventValues.at(EventInds::CLK_UNHALTED_IND) -
+        static_cast<double>(gEventValues.at(EventInds::CLK_UNHALTED_IND) -
         gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND)) /
         static_cast<double>(gEventValues.at(EventInds::CLK_UNHALTED_IND));
     }
@@ -154,13 +152,12 @@ int haswell_membound_stall_cycles(metric_id_t metric_id,
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
 
-    *out_value= 0.0;
     using namespace MB;
 
     // The value out here is given as a fraction of active cycles
     if (ARM_MAP_MEMORY_BOUND) {
       *out_value=
-	static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND)) /
+        static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND)) /
         static_cast<double>(gEventValues.at(EventInds::CLK_UNHALTED_IND));
     }
     return 0;
@@ -172,16 +169,15 @@ int haswell_membound_store_buffer_stall_cycles(metric_id_t metric_id,
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
 
-    *out_value= 0.0;
     if (ARM_MAP_MEMORY_BOUND) {
       // The value out here is given as a fraction of active cycles
       *out_value=
-	static_cast<double>(MB::gEventValues.at(MB::EventInds::RESOURCE_STALLS_SB_IND)) /
+        static_cast<double>(MB::gEventValues.at(MB::EventInds::RESOURCE_STALLS_SB_IND)) /
         static_cast<double>(MB::gEventValues.at(MB::EventInds::CLK_UNHALTED_IND));
     } else {
       // The value out here is given as a fraction of stalled cycles
       *out_value=
-	static_cast<double>(BB::gEventValues.at(BB::EventInds::RESOURCE_STALLS_SB_IND)) /
+        static_cast<double>(BB::gEventValues.at(BB::EventInds::RESOURCE_STALLS_SB_IND)) /
         static_cast<double>(BB::gEventValues.at(BB::EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND));
     }
     return 0;
@@ -193,13 +189,12 @@ int haswell_membound_l1d_pending_stall_cycles(metric_id_t metric_id,
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
 
-    *out_value= 0.0;
     using namespace MB;
 
     // The value out here is given as a fraction of active cycles
     if (ARM_MAP_MEMORY_BOUND) {
       *out_value=
-	static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_STALLS_L1D_PENDING_IND)) /
+        static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_STALLS_L1D_PENDING_IND)) /
         static_cast<double>(gEventValues.at(EventInds::CLK_UNHALTED_IND));
     }
     return 0;
@@ -223,7 +218,6 @@ int haswell_membound_memory_bound(metric_id_t metric_id,
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
 
-    *out_value= 0.0;
     using namespace MB;
 
     if (ARM_MAP_MEMORY_BOUND) {
@@ -239,14 +233,14 @@ int haswell_membound_l1d_pend_miss_fb_full_cycles(metric_id_t metric_id,
 {
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
-    *out_value= 0.0;
+
     using namespace BB;
 
     if (!ARM_MAP_MEMORY_BOUND) {
       // The value out here is given as a fraction of STALLED cycles
       *out_value=
-	static_cast<double>(gEventValues.at(EventInds::L1D_PEND_MISS_FB_FULL_IND))/
-	static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND));
+        static_cast<double>(gEventValues.at(EventInds::L1D_PEND_MISS_FB_FULL_IND))/
+        static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND));
     }
 
     return 0;
@@ -257,14 +251,14 @@ int haswell_membound_offcore_requests_buffer_sq_cycles(metric_id_t metric_id,
 {
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
-    *out_value= 0.0;
+
     using namespace BB;
 
     if (!ARM_MAP_MEMORY_BOUND) {
       // The value out here is given as a fraction of STALLED cycles
       *out_value=
-	static_cast<double>(gEventValues.at(EventInds::OFFCORE_REQUESTS_BUFFER_SQ_IND))/
-	static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND));
+        static_cast<double>(gEventValues.at(EventInds::OFFCORE_REQUESTS_BUFFER_SQ_IND))/
+        static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND));
     }
 
     return 0;
@@ -287,7 +281,7 @@ int haswell_membound_bandwidth_bound(metric_id_t metric_id,
 {
     // Update the counter values at the current sample period
     update_values(metric_id, current_sample_time);
-    *out_value= 0.0;
+
     using namespace BB;
 
     if (!ARM_MAP_MEMORY_BOUND) {
@@ -299,42 +293,6 @@ int haswell_membound_bandwidth_bound(metric_id_t metric_id,
     return 0;
 }
 
-int haswell_membound_latency_bound(metric_id_t metric_id,
-        struct timespec *current_sample_time, double *out_value)
-{
-    // Update the counter values at the current sample period
-    update_values(metric_id, current_sample_time);
-
-    *out_value= 0.0;
-    // this metric will have to be derived after combining the sets of metrics
-    // in a data format outside of map
-#if 0
-    // The value out here is given as a fraction of stalled cycles
-    *out_value= static_cast<double>(memory_bound_measure() - bandwidth_bound_measure()) /
-        gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND);
-#endif
-
-    return 0;
-}
-
-int haswell_membound_other_stall_reason(metric_id_t metric_id,
-        struct timespec *current_sample_time, uint64_t *out_value)
-{
-    // Update the counter values at the current sample period
-    update_values(metric_id, current_sample_time);
-    *out_value= 0;
-
-    // this metric will have to be derived after combining the sets of metrics
-    // in a data format outside of map
-#if 0
-    // The value out here is given as a fraction of active cycles
-    *out_value= static_cast<double>(gEventValues.at(EventInds::CYCLE_ACTIVITY_NO_EXECUTE_IND) -
-            memory_bound_measure()) /
-        gEventValues.at(EventInds::CLK_UNHALTED_IND);
-#endif
-
-    return 0;
-}
 } // extern "C"
 
 //! Returns the thread id of the calling thread
@@ -348,7 +306,7 @@ static unsigned long int haswell_membound_get_thread_id()
  */
 template<int NI>
 int get_event_codes(std::array<int, NI> & eventCodes,
-		    const std::array<const char*, NI> eventNames)
+                    const std::array<const char*, NI> eventNames)
 {
   // Get the event codes for the string descriptors
   // Begin by initialising the event codes to some known invalid value
@@ -360,7 +318,7 @@ int get_event_codes(std::array<int, NI> & eventCodes,
   for (const auto& name : eventNames) {
     int eventCode= PAPI_NULL;
     if (PAPI_event_name_to_code(const_cast<char*>(name), &eventCode)
-	!= PAPI_OK) {
+        != PAPI_OK) {
       //  TODO: Add some actual error handling in here. For the time
       //  being we just ignore any issue with the counter name
       printf("PAPI_NOT_OK!\n");
@@ -439,19 +397,19 @@ int haswell_membound_initialise_papi(plugin_id_t plugin_id)
     // Get the event codes for the string descriptors
     if (ARM_MAP_MEMORY_BOUND)
       get_event_codes<MB::EventInds::NUM_INDS>
-	(MB::gEventCodes, MB::gEventNames);
+        (MB::gEventCodes, MB::gEventNames);
     else
       get_event_codes<BB::EventInds::NUM_INDS>
-	(BB::gEventCodes, BB::gEventNames);
+        (BB::gEventCodes, BB::gEventNames);
 
     return 0;
 }
 
 template<int NI>
 int initialize_events(int * eventSetPtr, plugin_id_t plugin_id,
-		      std::array<int, NI> & eventCodes,
-		      const std::array<const char*, NI> eventNames,
-		      std::array<long long, NI> & eventValues)
+                      std::array<int, NI> & eventCodes,
+                      const std::array<const char*, NI> eventNames,
+                      std::array<long long, NI> & eventValues)
 {
   // Create the event sets
   int retval = PAPI_create_eventset(eventSetPtr);
@@ -462,20 +420,20 @@ int initialize_events(int * eventSetPtr, plugin_id_t plugin_id,
 
   // We assume that all of the events have been found at this point
   retval= PAPI_add_events(*eventSetPtr, eventCodes.data(),
-			  eventCodes.size());
+                          eventCodes.size());
   if (retval != PAPI_OK) {
     if (retval > 0) {
       printf("Error adding events to the event set. Last successful event added \"%s\".\n",
-	     eventNames[retval-1]);
+             eventNames[retval-1]);
       
       allinea_set_plugin_error_messagef(plugin_id, retval,
-					"Error adding events to the event set. Last successful "
-					"event added: \"%s\"\n.",
-					eventNames[retval-1]);
+                                        "Error adding events to the event set. Last successful "
+                                        "event added: \"%s\"\n.",
+                                        eventNames[retval-1]);
     } else {
       allinea_set_plugin_error_messagef(plugin_id, retval, "Error "
-					"adding events to the event set: %s",
-					PAPI_strerror(retval));
+                                        "adding events to the event set: %s",
+                                        PAPI_strerror(retval));
     }
     return ERROR;
   }
@@ -505,16 +463,16 @@ extern "C" {
             return ERROR;
         }
 
-	if (ARM_MAP_MEMORY_BOUND)
-	  initialize_events<MB::EventInds::NUM_INDS>(&gEventSet, plugin_id,
-						     MB::gEventCodes,
-						     MB::gEventNames,
-						     MB::gEventValues);
-	else
-	  initialize_events<BB::EventInds::NUM_INDS>(&gEventSet, plugin_id,
-						     BB::gEventCodes,
-						     BB::gEventNames,
-						     BB::gEventValues);
+        if (ARM_MAP_MEMORY_BOUND)
+          initialize_events<MB::EventInds::NUM_INDS>(&gEventSet, plugin_id,
+                                                     MB::gEventCodes,
+                                                     MB::gEventNames,
+                                                     MB::gEventValues);
+        else
+          initialize_events<BB::EventInds::NUM_INDS>(&gEventSet, plugin_id,
+                                                     BB::gEventCodes,
+                                                     BB::gEventNames,
+                                                     BB::gEventValues);
         return 0;
     }
 
@@ -526,26 +484,26 @@ extern "C" {
         // Stop the event set counting
       int retval;
       if (ARM_MAP_MEMORY_BOUND)
-	retval= PAPI_stop(gEventSet, MB::gEventValues.data());
+        retval= PAPI_stop(gEventSet, MB::gEventValues.data());
       else
-	retval= PAPI_stop(gEventSet, BB::gEventValues.data());
+        retval= PAPI_stop(gEventSet, BB::gEventValues.data());
 
       if (retval != PAPI_OK) {
-	allinea_set_plugin_error_messagef(plugin_id, retval, "Error in PAPI_stop: %s", PAPI_strerror(retval));
-	return ERROR;
+        allinea_set_plugin_error_messagef(plugin_id, retval, "Error in PAPI_stop: %s", PAPI_strerror(retval));
+        return ERROR;
       }
       // Remove all events from the event set
       retval = PAPI_cleanup_eventset(gEventSet);
       if (retval != PAPI_OK) {
-	allinea_set_plugin_error_messagef(plugin_id, retval, "Error in PAPI_cleanup_eventset: %s", PAPI_strerror(retval));
-	return ERROR;
+        allinea_set_plugin_error_messagef(plugin_id, retval, "Error in PAPI_cleanup_eventset: %s", PAPI_strerror(retval));
+        return ERROR;
       }
 
       // Destroy the event set
       retval = PAPI_destroy_eventset(&gEventSet);
       if (retval != PAPI_OK) {
-	allinea_set_plugin_error_messagef(plugin_id, retval, "Error in PAPI_destroy_eventset: %s", PAPI_strerror(retval));
-	return ERROR;
+        allinea_set_plugin_error_messagef(plugin_id, retval, "Error in PAPI_destroy_eventset: %s", PAPI_strerror(retval));
+        return ERROR;
       }
 
       // Reset the event set
